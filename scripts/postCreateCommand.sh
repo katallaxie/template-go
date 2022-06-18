f9244a24219317105700ffabdbfa5ec2a1a9bb3b
Sebastian Döll <sebastian@katallaxie.me>
2022-06-18 09:38:30 +0000

    adding Dockerfile and new post create steps

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # This script is executed after the creation of a new project.
 
-apt-get install -y direnv
+echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | sudo tee /etc/apt/sources.list.d/goreleaser.list
+sudo apt update
+sudo apt install -y goreleaser direnv
 
 cp scripts/pre-commit.sh .git/hooks/pre-commit
 cp scripts/pre-push.sh .git/hooks/pre-push
