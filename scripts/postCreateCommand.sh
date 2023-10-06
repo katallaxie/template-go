74fd0eda54e45b50079ae8dd735e6dd253242a83
Sebastian Döll <sebastian@katallaxie.me>
2023-10-06 07:30:08 +0000

    chore: update go1.21

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 # This script is executed after the creation of a new project.
-
-cp ./scripts/pre-commit.sh .git/hooks/pre-commit
-cp ./scripts/pre-push.sh .git/hooks/pre-push
-chmod 755 .git/hooks/pre-commit
-chmod 755 .git/hooks/pre-push
