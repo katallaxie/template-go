452d47077c5384e93b19e43b0854f5ba4df5118b
Sebastian Döll <sebastian@katallaxie.dev>
2025-03-05 09:01:35 +0100

    chore: update goreleaser@v2

--- tools/tools.go
+++ tools/tools.go
@@ -5,7 +5,7 @@ package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/goreleaser/goreleaser"
+	_ "github.com/goreleaser/goreleaser/v2"
 	_ "github.com/katallaxie/pkg/cmd/runproc"
 	_ "github.com/vektra/mockery/v2"
 	_ "gotest.tools/gotestsum"
